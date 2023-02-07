e5ca025122ab48c0a35b18a3008e475be58f835e
Vitor Franco <vitoroliveirafranco@gmail.com>
2023-02-07 17:18:17 -0300

    feat: adiciona validacao e tipagem das envs

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,12 @@
 import fastify from 'fastify'
 import crypto from 'node:crypto'
 import { knex } from './database'
+import { env } from './env'
 
 const app = fastify()
 
 app.get('/hello', async () => {
-  const transaction = await knex('transactions')
+  await knex('transactions')
     .insert({
       id: crypto.randomUUID(),
       title: 'transação de teste',
@@ -18,7 +19,7 @@ app.get('/hello', async () => {
 
 app
   .listen({
-    port: 3333,
+    port: env.PORT,
   })
   .then(() => {
     console.log('HTTP Server is running')
